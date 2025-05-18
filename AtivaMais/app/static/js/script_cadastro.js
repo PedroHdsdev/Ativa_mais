ff11a54c121a3699feb265e94f8cbe3884510409
PEDROHDS <pedro.henriquesil@hotmail.com>
2025-05-17 23:53:39 -0300

    HOME/CADASTRO

---
+++ AtivaMais/app/static/js/script_cadastro.js
@@ -0,0 +1,7 @@
+ function escolherPerfil(perfil) {
+    // define valor do hidden
+    document.getElementById("perfil").value = perfil;
+
+    // esconde o modal
+    document.getElementById("myModal_cad").style.display = "none";  
+  }
